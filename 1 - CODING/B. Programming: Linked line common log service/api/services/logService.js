e00bb105f4efeb624ed0f7ea0cb89d3a3fa843f9
Julio <mascittija@gmail.com>
2020-09-19 16:06:14 -0300

    avoiding load the file, read line by line until lastone

--- 1 - CODING/B. Programming: Linked line common log service/api/services/logService.js
+++ 1 - CODING/B. Programming: Linked line common log service/api/services/logService.js
@@ -3,17 +3,42 @@ const fs = require('fs'); // file system module
 const path = require('path');
 const filePath = path.join(__dirname, '../public/log.csv');
 
+const readline = require('readline');
+const Stream = require('stream');
+
 // Mantengo registro de la úlima línea del log
 let lastLog = '';
 
-// Ultima línea del CSV
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) throw err;
-    const lines = data.trim().split('\n');
-    const lastLine = lines.slice(-1)[0];
-    lastLog = lastLine;
-    console.log("Last line of file: ",lastLine);
-});
+// Leo la última línea evitando cargar en memoria todo el archivo.
+function getLastLine()  {
+    const inStream = fs.createReadStream(filePath);
+    const outStream = new Stream;
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface(inStream, outStream);
+
+        let lastLine = '';
+        rl.on('line', function (line) {
+            if (line.length > 0) {
+                lastLine = line;
+            }
+        });
+
+        rl.on('error', reject)
+
+        rl.on('close', function () {
+            resolve(lastLine)
+        });
+    })
+}
+
+getLastLine()
+    .then(lastLine => {
+        lastLog = lastLine;
+        console.log("Last line of file: ",lastLine);
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
 function getLastLog(){
     return lastLog;
